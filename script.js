2b576192a9e377ddeb577f8780943aa3eaeaa455
hiroki0104 <hiroki010419@outlook.jp>
2020-04-10 18:49:26 +0900

    5x5_change_pazzle

--- script.js
+++ script.js
@@ -7,12 +7,12 @@
 
   function init() {
     const table = document.getElementById('table')
-    for (let i = 0; i < 4; ++i) {
+    for (let i = 0; i < 5; ++i) {
       const tr = document.createElement('tr')
 
-      for (let j = 0; j < 4; ++j) {
+      for (let j = 0; j < 5; ++j) {
         const td = document.createElement('td')
-        const index = i * 4 + j
+        const index = i * 5 + j
         td.className = 'tile'
         td.index = index
         td.value = index
@@ -26,29 +26,38 @@
     }
 
     for (let i = 0; i < 1000; ++i) {
-      click({ srcElement: { index: Math.floor(Math.random() * 16) } })
+      click({ srcElement: { index: Math.floor(Math.random() * 25) } })
     }
   }
 
+  //   indexの条件分岐
   function click(e) {
     const i = e.srcElement.index
 
-    if (i - 4 >= 0 && tiles[i - 4].value === 0) {
-      swap(i, i - 4)
-    } else if (i + 4 < 16 && tiles[i + 4].value === 0) {
-      swap(i, i + 4)
-    } else if (i % 4 !== 0 && tiles[i - 1].value === 0) {
+    // 下のタイル
+    if (i - 5 >= 0 && tiles[i - 5].value === 0) {
+      swap(i, i - 5)
+    }
+    // 上のタイル
+    else if (i + 5 < 25 && tiles[i + 5].value === 0) {
+      swap(i, i + 5)
+    }
+    // 左のタイル
+    else if (i % 5 !== 0 && tiles[i - 1].value === 0) {
       swap(i, i - 1)
-    } else if (i % 4 !== 3 && tiles[i + 1].value === 0) {
+    }
+    // 右のタイル
+    else if (i % 5 !== 4 && tiles[i + 1].value === 0) {
       swap(i, i + 1)
     }
   }
 
+  //   タイルの表示される文字とvalueを入れ替える
   function swap(i, j) {
-    const tmp = tiles[i].value
-    tiles[i].textContent = tiles[j].textContent
+    const beforetmp = tiles[i].value
+    tiles[i].textContent = ''
     tiles[i].value = tiles[j].value
-    tiles[j].textContent = tmp
-    tiles[j].value = tmp
+    tiles[j].textContent = beforetmp
+    tiles[j].value = beforetmp
   }
 })()
